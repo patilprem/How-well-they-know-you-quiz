bd0b9a5198f5946effcd182f7e141d66392bc3d7
Prem Patil <>
2021-04-10 06:36:28 +0000

    Quiz

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
+//QUIZ
+
+var questions = [
+  {
+  question: "Who is my favorite superhero? ",
+  answer: "ironman"
+  },
+
+  {
+  question: "Which is my favorite dish? ",
+  answer: "biryani"
+  },
+  
+  {
+  question: "Which is my favorite color? ",
+  answer: "black"
+  },
+
+  {
+  question: "Who is my favorite cricketer? ",
+  answer: "kohli"
+  },
+
+  {
+  question: "Who is my favorite actor ? ",
+  answer: "hritik"
+  }
+
+]
+
 var readlineSync = require("readline-sync");
+console.log("Hello " + readlineSync.question("What is your name? ") + ", Welocme to DO YOU KNOW PREM quiz")
+console.log("---------------------------") 
 var score = 0
-var userAnswer = readlineSync.question("What is your Name? ");
-console.log("Welocome to programming " + userAnswer);
-var userAge = readlineSync.question("What is your age? ");
-if(userAge > 25)
+//console.log(questions[0].question)
+function quiz(question, answer){
+  var userAnswer = readlineSync.question(question);
+  if(userAnswer === answer)
   { console.log("You're right");
     score = score + 1;
   }
-else if(score != 0)
+  else if(score != 0)
   { console.log("You're Wrong");
     score = score - 1;
   }
-else {console.log("You're Wrong")};  
-var userCity = readlineSync.question("What is your hometown? ");
-if(userCity === "Mumbai")
- { console.log("You're right");
-    score = score + 1;
-  }
-else if(score != 0)
-  { console.log("You're Wrong");
-    score = score - 1;
-  }
-else{console.log("You're Wrong")};    
-console.log("-----------------------------");
-console.log("Your final score is " + score);
+  else {console.log("You're Wrong")};
+  console.log("Your score is: " + score);
+  console.log("---------------------------")  
+};
+
+for(var i=0; i<questions.length; i++){
+  
+  quiz(questions[i].question, questions[i].answer)
+  
+};
